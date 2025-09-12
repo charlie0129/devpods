9b6441dbfcb4d491700e9f1fdabc94749bddc973
Charlie Chiang <charlie_c_0129@outlook.com>
2025-09-12 07:24:53 +0000

    fix startup issue on nfs root squash pvc

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -10,7 +10,7 @@ if [[ -d "/workspaces" ]]; then
     # is used as home, so user's changes in home are preserved across restarts.
 
     # We will edit the home dir manually instead of using usermod (usermod prevents us from editing logged-in user).
-    install -d -m 700 -o root -g root "/workspaces/root"
+    install -d -m 700 -o root -g root "/workspaces/root" || mkdir -p "/workspaces/root" # On NFS PVC it may fail with permission issues due to root squash, so we skip chown if install fails.
     awk -F: -vOFS=: -v h="/workspaces/root" '$1=="root"{$6=h}1' /etc/passwd >/etc/passwd.new
     install -o root -g root -m "$(stat -c '%a' /etc/passwd)" /etc/passwd.new /etc/passwd
     rm -f /etc/passwd.new
@@ -18,7 +18,7 @@ if [[ -d "/workspaces" ]]; then
     head -1 /etc/passwd 
     
     # Copy home
-    cp -a "/root/." "/workspaces/root"
+    cp -a "/root/." "/workspaces/root" || cp -dfR "/root/." "/workspaces/root" # Fallback on NFS PVC root squash.
 
     export HOME=/workspaces/root
     # Bootstrap dotfiles again to fix home dir changes.
