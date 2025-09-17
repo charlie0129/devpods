73795199bf5d6030f149991e311b6d8400a8787b
Charlie Chiang <charlie_c_0129@outlook.com>
2025-09-17 08:27:33 +0000

    update scripts

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -10,7 +10,12 @@ if [[ -d "/workspaces" ]]; then
     # is used as home, so user's changes in home are preserved across restarts.
 
     # We will edit the home dir manually instead of using usermod (usermod prevents us from editing logged-in user).
-    install -d -m 700 -o root -g root "/workspaces/root" || mkdir -p "/workspaces/root" # On NFS PVC it may fail with permission issues due to root squash, so we skip chown if install fails.
+    install -d -m 700 -o root -g root "/workspaces/root" || {
+        # Fallback on NFS PVC root squash.
+        echo "Your PVC does not allow chown. Using mkdir..."
+        # On NFS PVC it may fail with permission issues due to root squash, so we skip chown if install fails.
+        mkdir -p "/workspaces/root"
+    } 
     awk -F: -vOFS=: -v h="/workspaces/root" '$1=="root"{$6=h}1' /etc/passwd >/etc/passwd.new
     install -o root -g root -m "$(stat -c '%a' /etc/passwd)" /etc/passwd.new /etc/passwd
     rm -f /etc/passwd.new
@@ -18,7 +23,11 @@ if [[ -d "/workspaces" ]]; then
     head -1 /etc/passwd 
     
     # Copy home
-    cp -a "/root/." "/workspaces/root" || cp -dfR "/root/." "/workspaces/root" # Fallback on NFS PVC root squash.
+    cp -a "/root/." "/workspaces/root" || {
+        # Fallback on NFS PVC root squash.
+        echo "Your PVC does not allow chown. Using regular cp..."
+        cp -dfR "/root/." "/workspaces/root"
+    }
 
     export HOME=/workspaces/root
     # Bootstrap dotfiles again to fix home dir changes.
@@ -26,6 +35,11 @@ if [[ -d "/workspaces" ]]; then
 else
     echo "Skip change home dir because /workspaces is not present, i.e., you are not in DevPod."
 fi
+
+echo "Allowing * as safe git directory"
+# Allow using any git dir. Since NFS root squash may end up with nobody:nogroup, we have to do this to use git.
+git config --global --unset-all safe.directory || true # May fail with empty git config
+git config --global --add safe.directory '*'
 
 # Start docker
 /usr/local/bin/dockerd
